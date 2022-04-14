b44de14d30145e9a3656041798eb54851084155f
Conrad Zelck <git@simpel.cc>
2022-04-14 17:03:59 +0200

    feat: introduce global head/header
    Signed-off-by: Conrad Zelck <git@simpel.cc>

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php include dirname($_SERVER['DOCUMENT_ROOT']) . "/simpel.cc/php/head.php"; ?>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+
+<body>
+  <header>
+		<?php include dirname($_SERVER['DOCUMENT_ROOT']) . "/simpel.cc/php/header.php"; ?>
+	</header>
+
+  <main>
+    <noscript>
+      <b>Warning: JavaScript had to be enabled.</b>
+      <br><br>
+    </noscript>
+
+    <div id="center">
+      <div class="text">This browser appears to be</div>
+      <div class="text" id="browsertext"></div>
+      <div class="wrapper">
+        <div class="browser chrome"></div>
+        <div class="browser edge"></div>
+        <div class="browser firefox"></div>
+        <div class="browser msie"></div>
+        <div class="browser opera"></div>
+        <div class="browser safari"></div>
+      </div>
+      <details class="agent">
+        <summary class="agent">more infos</summary>
+        <p class="agent" id="agent"></p>
+      </details>
+    </div>
+  </main>
+
+  <script src="script.js"></script>
+</body>
+
+</html>
---
+++ localSettingsHeader.php
@@ -0,0 +1,6 @@
+<?php
+  define("SITE_TITLE", "Browser Detector");
+  define("SITE_COPYRIGHT_CONTENT", "MIT 2021 Simpel");
+  define("SITE_H1", "Browser Detector");
+  define("SITE_GITHUB", "https://github.com/SimpelMe/browser-detector");
+?>
